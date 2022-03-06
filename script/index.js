deaba220a4e6a34cf041d4747f5630d3cb561a76
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-06 03:19:09 +0300

    fix:remove redudant form  elements

---
+++ script/index.js
@@ -0,0 +1,35 @@
+var year = document.getElementById("birthYear");
+var month = document.getElementById("birthMonth");
+var day = document.getElementById("birthDay");
+var submit = document.getElementById("submit-btn");
+
+
+
+function calculateDay(){
+    let CC = year.value.slice(0,2);
+    let YY = year.value.slice(2);
+    let MM = month.value;
+    let DD = day.value;
+    if ((CC == 0) && (YY==0)){
+        alert ("Enter a valid year")
+    }
+    else if (DD<=0){
+        alert("Enter a valid birthday")
+    }
+    else{
+        let day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    }
+   
+    return day.toFixed()
+}
+function getAkanName(){
+    let gender = document.getElementById("gender").value;
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] 
+    let findNameIndex = calculateDay();
+
+}
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+    calculateDay()
+})
