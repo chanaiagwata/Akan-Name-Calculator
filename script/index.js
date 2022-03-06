57476231bdb5b2b30f2f517a99ca64eb16ca2d00
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-06 16:45:06 +0300

    feat: add form validation

--- script/index.js
+++ script/index.js
@@ -3,36 +3,51 @@ var month = document.getElementById("birthMonth");
 var day = document.getElementById("birthDay");
 var submit = document.getElementById("submit-btn");
 
-function calculateDay(){
-    let CC = year.value.slice(0,2);
+function calculateDay() {
+    let CC = year.value.slice(0, 2);
     let YY = year.value.slice(2);
     let MM = month.value;
     let DD = day.value;
-    let akanDay = Math.floor((CC/4 -2*CC-1 + (5*YY/4) + (26*(MM+1))/10) + DD ) % 7;
+    let akanDay = Math.floor((CC / 4 - 2 * CC - 1 + (5 * YY / 4) + (26 * (MM + 1)) / 10) + DD) % 7;
 
     return akanDay;
 }
-function getAkanName(){
+
+function getAkanName() {
     let gender = document.getElementById("gender")
     console.log(gender.value);
-    let index =calculateDay();
-    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
-        let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let index = calculateDay();
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-        if (gender.value == "female"){
-            let name = femaleNames[index]
-            document.getElementById("output").innerHTML = name;
 
-        }
-        else{
-            let name = maleNames[index]
-            document.getElementById("output").innerHTML = name;
-        }
-   
+    if (gender.value == "female") {
+        let name = femaleNames[index]
+        document.getElementById("output").innerHTML = name;
+
+    } else {
+        let name = maleNames[index]
+        document.getElementById("output").innerHTML = name;
+    }
+
 }
 
-submit.addEventListener("click", function(e){
+function formValidate() {
+    if (year.value == "") {
+        alert("Enter a valid year")
+    } else if (month.value < "1") {
+        alert("Enter a valid month")
+    } else if (day.value == "") {
+        alert("enter a valid day")
+    } else {
+        getAkanName();
+
+    }
+
+}
+
+submit.addEventListener("click", function (e) {
     e.preventDefault();
-      getAkanName(); 
+    formValidate();
 
 })
