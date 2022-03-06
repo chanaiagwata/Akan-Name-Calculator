df063c0c18f5f5cf820592426049e858d153c8ee
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-03-06 15:00:09 +0300

    refactor:implement calculation method for successive operation

--- script/index.js
+++ script/index.js
@@ -2,42 +2,37 @@ var year = document.getElementById("birthYear");
 var month = document.getElementById("birthMonth");
 var day = document.getElementById("birthDay");
 var submit = document.getElementById("submit-btn");
-var output = document.getElementById("output");
-
 
 function calculateDay(){
     let CC = year.value.slice(0,2);
     let YY = year.value.slice(2);
     let MM = month.value;
     let DD = day.value;
-    if ((CC == 0) && (YY==0)){
-        alert ("Enter a valid year")
-    }
-    else if (DD<=0){
-        alert("Enter a valid birthday")
-    }
-    else{
-        let day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    }
-   
-    return day.toFixed()
+    let akanDay = Math.floor((CC/4 -2*CC-1 + (5*YY/4) + (26*(MM+1))/10) + DD ) % 7;
+
+    return akanDay;
 }
 function getAkanName(){
-    let gender = document.getElementById("gender").value;
-    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let gender = document.getElementById("gender")
+    console.log(gender.value);
+    let index =calculateDay();
     let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
-    let findNameIndex = calculateDay();
-    if (gender== "male"){
-        let mName = maleNames[findNameIndex];
-    console.log(mName);
-    }
-    else{
-        let fName = femaleNames[findNameIndex];
-console.log(fName);
-    }
+        let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
+        if (gender.value == "female"){
+            let name = femaleNames[index]
+            document.getElementById("output").innerHTML = name;
+
+        }
+        else{
+            let name = maleNames[index]
+            document.getElementById("output").innerHTML = name;
+        }
+   
 }
+
 submit.addEventListener("click", function(e){
     e.preventDefault();
-    calculateDay()
+      getAkanName(); 
+
 })
